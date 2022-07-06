6e7e8dd5c5e4a15374b42a584d3d05d60ed1396b
컨트롤D <48048511+copecone@users.noreply.github.com>
2022-07-06 19:33:10 +0900

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -4,21 +4,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register("./sw.js")
-            .then(res => console.log("Service Worker Registered!"))
+            .then(res => {
+                console.log("Service Worker Registered!")
+            
+                //Service Part
+                chatFrames = document.querySelector("#chat-frames")
+                
+                console.log(chatFrames);
+
+                youtubeChat = chatFrames.querySelector(".youtube-chat")
+                twitchChat = chatFrames.querySelector(".twitch-chat")
+
+                const query_args = new Proxy(new URLSearchParams(window.location.search), {
+                    get: (searchParams, prop) => searchParams.get(prop),
+                });
+
+                youtubeChat.src = `https://www.youtube.com/live_chat?is_popout=1&v=${query_args.youtube}&embed_domain=${window.location.host}`
+                twitchChat.src = `https://www.twitch.tv/embed/${query_args.twitch}/chat?parent=${window.location.host}`
+            })
             .catch(err => console.log(err))
     }
 
-    chatFrames = document.querySelector("#chat-frames")
-
-    console.log(chatFrames);
-
-    youtubeChat = chatFrames.querySelector(".youtube-chat")
-    twitchChat = chatFrames.querySelector(".twitch-chat")
-
-    const query_args = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-    });
-
-    youtubeChat.src = `https://www.youtube.com/live_chat?is_popout=1&v=${query_args.youtube}&embed_domain=${window.location.host}`
-    twitchChat.src = `https://www.twitch.tv/embed/${query_args.twitch}/chat?parent=${window.location.host}`
+    
 })
